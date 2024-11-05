d82c3d0a0767f03520e15717bab453f7b74c45a8
Lukas Boehm <lukas.l.boehm@fau.de>
2024-11-05 14:34:18 +0100

    fix

--- find.sql
+++ find.sql
@@ -7,10 +7,12 @@ WITH missing AS (SELECT itemID
                  from itemAttachments
                  WHERE syncState == 1
                    and linkMode == 1)
-SELECT *
+SELECT items.key, items.itemId, itemDataValues.value, itemAttachments.path
 FROM itemData
          LEFT JOIN itemDataValues on itemData.valueID = itemDataValues.valueID
-WHERE itemID IN missing
-  AND itemDataValues.value LIKE "http%"
+         LEFT JOIN items on itemData.itemID = items.itemID
+         LEFT JOIN itemAttachments on items.itemID = itemAttachments.itemID
+WHERE itemData.itemID IN missing
+  AND itemDataValues.value LIKE "http%" -- all links, although only arxiv might be smarter
   AND fieldID = 13 -- URL
 ;
